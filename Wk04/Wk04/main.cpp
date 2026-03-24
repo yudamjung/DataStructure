62a38a06193f465c53ef1335bfec7a9e8a994a0a
Yudam Jung <udamee1025@gmail.com>
2026-03-24 14:32:21 +0900

    동적 메모리 할당 (학생 수 입력받아 성적 평균 구하기)

--- Wk04/Wk04/main.cpp
+++ Wk04/Wk04/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    char s1[] = "String";
+int main () {
+
+    // 학생 수 입력 받기
+    int st;
+    cout << "학생 수를 입력하세요 : ";
+    cin >> st;
     
-    cout << strlen(s1) << endl;     // 문자열의 길이 반환
-
-
+    // 학생 성적 입력 받기
+    int *score = new int [st];        // 동적 메모리 할당
+        // == int score[st];
+    for (int i=0; i<st; i++) {
+        cin >> *(score+i);
+        // == cin >> score[i];
+    }
+    
+    // 학생 성적 출력하기
+    cout << "=== 입력된 학생의 성적 ===" << '\n';
+    for (int i=0; i<st; i++) {
+        cout << "학생" << i+1<< " 의 성적은 : " << score[i] << endl;
+    }
+    
+    // 평균 구하기
+    int sum = 0;
+    for (int i=0; i<st; i++) {
+        sum += *(score+i);
+        // == sum += score[i];
+    }
+    
+    cout << "=== 평균 성적 ===" << '\n';
+    cout << "성적 평균은 : " << (double)sum / st << endl;
+    
     return 0;
 }
