1e48ad76f8bbcbb367f3572c7bb72c96db416aad
Yudam Jung <udamee1025@gmail.com>
2026-03-24 12:46:22 +0900

    실습2

--- Wk04/Wk04/main.cpp
+++ Wk04/Wk04/main.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int main() {
     int score[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int* ptr;
     
-    // score == &score == &score[0] : 배열명 == &배열명 == &배열첫원소
-    cout << score << ' ' << &score << ' ' << &score[0] << '\n' << '\n';
+    ptr = score; // int* ptr = &score[0]
+    cout << ptr << '\n' << '\n';
     
     for (int i=0; i<10; i++) {
-        // sizeof(int) 만큼 주소 증가
-        // &score[n] == score + n. n = sizeof(dataType)
-        cout << &score[i] << " : " << score + i << endl;        // 주소에 접근
-        cout << score[i] << " : " << *(score + i) << endl;      // 값에 접근
-        cout << '\n';
+        cout << ptr + i << " : " << score + i << endl;              // 주소에 접근
+        cout << ptr[i] << " : " << *(ptr+i)<< " : " << score[i]  << " : " << *(score + i) << endl;                // 값에 접근
     }
     
     return 0;
