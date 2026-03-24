54369e5a650afec977e1b30f42859986671f67ee
Yudam Jung <udamee1025@gmail.com>
2026-03-24 14:00:10 +0900

    String Array

--- Wk04/Wk04/main.cpp
+++ Wk04/Wk04/main.cpp
@@ -2,24 +2,15 @@
 using namespace std;
 
 int main() {
-    // 2 Dimension Array w. pointer
-    int score[2][3] = { {10,20,30}, {40,50,60}};
+    char s1[] = "This is my world.";
+    char s2[20] = {'M','y', ' ', 'H', 'o', 'm', 'e', '\0'};
     
-//    for (int i=0; i<2; i++) {
-//        for (int j=0; j<3; j++) {
-//            cout << "Address of 2D Array : " << &score[i][j] << endl;
-//            cout << "Value of 2D Array : " << score[i][j] << endl;
-//        }
-//    }
-    
-    cout << score[0] << ' ' << *(score) << endl;       // 2차원 배열의 첫 번째 행 배열의 주소 반환
-    cout << score[1] << ' ' << *(score+1) << endl;       // 2차원 배열의 두 번째 행 배열의 주소 반환. 컴파일러는 좌측 코드를 우측처럼 해석
-    
-    cout << *(score + 1) << endl;
-    cout << *(*(score + 1) + 1) << endl;        // score[1][1]
-    cout << *(score[1] + 1) << endl;            // score[1][1]
-    cout << **score << endl;                    // score[0][0]
-    
-    
+    // 문자형이 아닌 배열 출력 시에는 배열명을 출력할 때 배열의 주소가 출력됐음
+    // 문자형인 배열에서는 null을 만날 때까지 출력
+    // s2의 남은 공간은 모두 null 초기화
+
+    cout << s1 << endl;
+    cout << s2 << endl;
+
     return 0;
 }
