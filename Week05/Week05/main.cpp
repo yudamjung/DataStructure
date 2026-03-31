124e14b84857de6b378c06ee1e1b3b7c6f4defc1
Yudam Jung <udamee1025@gmail.com>
2026-03-31 13:13:30 +0900

    과제1 연결리스트 만들고 출력

---
+++ Week05/Week05/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* link;
+};
+
+Node* head = NULL;
+Node* p1, *p2, *p3;
+
+int main() {
+    p1 = new Node;
+    p1->data = 10;
+    p1->link = NULL;
+    
+    p2 = new Node;
+    p2->data = 20;
+    p2->link = NULL;
+    p1->link = p2;      // p2가 생성된 뒤에 연결
+    
+    p3 = new Node;
+    p3->data = 30;
+    p3->link = NULL;    // 마지막이니까 NULL pointing
+    p2->link = p3;      // p3가 생성된 뒤에 연결
+    
+    head = p1;
+        
+    return 0;
+}
