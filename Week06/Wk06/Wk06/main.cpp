583bb4bc8828d188b9813d6ba8f21b601dfefc44
Yudam Jung <udamee1025@gmail.com>
2026-04-07 12:29:32 +0900

    반복적으로 사용자 입력값 받아 연결리스트의 마지막에 추가

--- Week06/Wk06/Wk06/main.cpp
+++ Week06/Wk06/Wk06/main.cpp
@@ -26,12 +26,25 @@ void insert_node_at_rear(Node *new_node) {
 void print_list() {
     cout << "전체 연결리스트" << endl;
     for (Node *list = head; list != NULL; list = list -> link) {  // ⭐️
-        cout << list->data << '\n' << '\n';
+        cout << list->data << '\n' ;
     }
 }
 
 int main() {
-    
-    
+    for (int i = 0; i< 8; i++) {
+        // 새로운 노드 할당 및 값 설정
+        Node *ptr = new Node;
+        
+        cout << "다음 데이터를 입력하세요: ";
+        int value;
+        cin >> value;
+        
+        ptr->data = value;
+        ptr->link = NULL;
+        
+        insert_node_at_rear(ptr);       // 전체 리스트의 뒤쪽에 새로운 값 추가
+    }
+    // 작업 확인용
+    print_list();
     return 0;
 }
