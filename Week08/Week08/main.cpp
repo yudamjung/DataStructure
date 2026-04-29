f5d0298d787e07573e1c4b6ce887b86579d568ce
Yudam Jung <udamee1025@gmail.com>
2026-04-29 13:10:06 +0900

    연결리스트로 구현한 스택 오류 수정 최종

--- Week08/Week08/main.cpp
+++ Week08/Week08/main.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 using namespace std;
 
-// 스택을 이용한 괄호 검사
+// 연결리스트로 구현하는 스택
+class Node {
+public:
+    int data;
+    Node *link;
+};
 
 class MyStack {
 public:
-    int data[100];
-    int top;
-    
-    MyStack() { top = -1; }
+    Node *top;                  // Stack Pointer, Pointer 변수, head 역할
+    MyStack() { top = NULL; }
     
     bool is_stack_empty() {
-        return (top == -1);
+        return (top == NULL);
     }
     
-    void push(int item) {
-        data[++top] = item;
+    void push(int item) {       // 단순 연결리스트에서 첫 노드 삽입하기
+        Node *new_node = new Node;
+        new_node -> data = item;
+        //new_node -> link = NULL;
+        
+        // 단순 연결 리스트의 첫 노드로 삽입
+        new_node -> link = top;
+        top = new_node;
     }
     
-    int pop () {
-        return data[top--];
+    int pop() {
+        if (is_stack_empty()) {
+            cout << "UNDERFLOW" << endl;
+            exit(1);
+        } else {
+            int value = top->data;
+            Node* oldTop = top;
+            top = top->link;
+            delete oldTop;
+            return value;
+        }
+    }
+    
+    // Stack 출력 함수
+    void stack_status () {
+        if (is_stack_empty()) {
+            cout << "Stack is empty" << '\n';
+            return;
+        }
+        else {
+            for (Node *list = top; list != NULL; list = list->link) {
+                cout << list->data;
+                if (list->link) cout << " -> ";
+            }
+        }
+        cout << '\n';
     }
 };
 
-bool check_matching(char str[]) {
+int main() {
     MyStack s1;
     
-    for (int i=0; i < strlen(str); i++) {       // 입력된 글자를 한 글자씩 순회
-        if (str[i] == '(' || str[i] == '[' || str[i] == '{') {        // 개괄호면 push
-            s1.push(str[i]);
-        }
-        else if (str[i] == ')') {
-            if (s1.pop() != '(') return false;
-        }
-        else if (str[i] == ']') {
-            if (s1.pop() != '[') return false;
-        }
-        else if (str[i] == '}') {
-            if (s1.pop() != '{') return false;
-        }
-    }
-    if (s1.is_stack_empty()) return true;
-    else return false;
-}
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.pop();       // 30을 pop
+    s1.push(40);
     
-int main() {
-    char str[100];
-    cin.getline(str, 100, '\n');    // (버퍼 시작 주소, 버퍼 최대 크기, 구분자)
-    
-    if (check_matching(str)) {
-        cout << "괄호 검사 성공" << endl;
-    }
-    else cout << "괄호 검사 실패" << endl;
-    
-    return 0;
+    s1.stack_status();
 }
