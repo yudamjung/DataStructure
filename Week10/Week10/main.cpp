504ab684c705dbfaf40cd55ff746001e2c6ff569
Yudam Jung <udamee1025@gmail.com>
2026-05-12 14:03:09 +0900

    Queue: 배열 방식 구현

--- Week10/Week10/main.cpp
+++ Week10/Week10/main.cpp
@@ -1,74 +1,73 @@
 #include <iostream>
 using namespace std;
 
-// 후위 표기식 계산 알고리즘
-// 1. 개괄호 만나면 무시
-// 2. 피연산자를 만나면 스택에 ㅔush
-// 3. 연산자를 만나면 필요한 개수의 피연산자를 스택에서 pop
-// 4. 연산결과를 다시 스택에 Push
-// 5. 수식이 끝나면 마지막으로 스택을 pop 하여 출력
+// Queue 배열 방식으로 구현
 
 const int MAX_SIZE = 100;
 
-class MyStack {
+class MyQueue {
 public:
     int data[MAX_SIZE];
-    int top;
+    int front, rear;
     
-    MyStack() {
+    MyQueue() {
         // constructor
-        top = -1;
+        front = rear = -1;
     }
     
-    void push(int item) {
-        // isStackFull 검사
-        data[++top] = item;
+    // Queue 가 꽉 찼는지 확인하는 메서드
+    bool is_queue_full() {
+//        if(rear = MAX_SIZE - 1) return true;
+//        else return false;
+        return (rear == MAX_SIZE - 1);
     }
     
-    int pop() {
-        // isStackEmpty 검사
-        return data[top--];
+    // Queue가 비어있는지 확인하는 메서드
+    bool is_queue_empty() {
+//        if (front == rear) return true;
+//        else return false;
+        return (front == rear);
     }
+    
+    // Queue에 원소를 삽입하는 메서드
+    void enQueue(int item) {
+        // Queue가 full 인지 검사
+        if (is_queue_full()) {
+            cout << "QUEUE OVERFLOW" << endl;
+        }
+        else data[++rear] = item;
+    }
+    
+    // Queue의 원소를 삭제하는 메서드
+    int deQueue() {
+        // Queue가 empty 인지 검사
+        if (is_queue_empty()) {
+            cout << "QUEUE UNDERFLOW" << endl;
+            exit(1);
+        }
+        else return data[++front];
+    }
+    
+    // Queue의 요소들을 출력하는 메서드
+    void queue_list() {
+        if (is_queue_empty()) { cout << "큐가 비어있습니다."; }
+        for (int i = front + 1; i <= rear; i++) {
+            cout << ' ' << data[i] << ' ';
+        }
+    }
+    
 };
 
 
 int main() {
-    MyStack s1;
-    char str[100];
-    cout << "후위 수식 입력 : ";
-    cin.getline(str, 100, '\n');
+    MyQueue q1;
     
-    for (int i=0; i<strlen(str); i++) {
-        char opr1, opr2;
-        char ch = str[i];
-        
-        if(ch == ' ') continue;     // 공백이 입력되는 경우 처리
-        if (ch != '+' && ch != '-' && ch != '*' && ch != '/') {     // 사칙연산자가 아니면 (피연산자이면)
-                s1.push(ch - '0');              // push => 아스키코드 6 -> 숫자문자 6
-        }
-        else if (ch == '+') {
-            opr2 = s1.pop();
-            opr1 = s1.pop();
-            s1.push(opr1 + opr2);
-        }
-        else if (ch == '-') {
-            opr2 = s1.pop();
-            opr1 = s1.pop();
-            s1.push(opr1 - opr2);
-        }
-        else if (ch == '*') {
-            opr2 = s1.pop();
-            opr1 = s1.pop();
-            s1.push(opr1 * opr2);
-        }
-        else if (ch == '/') {
-            opr2 = s1.pop();
-            opr1 = s1.pop();
-            s1.push(opr1 / opr2);
-        }
-    }
+    q1.enQueue(10);
+    q1.enQueue(20);
+    q1.enQueue(30);
+    q1.enQueue(40);
+    q1.deQueue();
     
-    cout << str << " = " << s1.pop() << endl;
-    
+    q1.queue_list();
     return 0;
 }
